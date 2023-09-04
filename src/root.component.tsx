60e71771708421cee817a989bb6a50ee38d02e11
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-09-03 23:48:06 -0300

    feat: get checkout info through redirect

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,13 @@
+/* eslint-disable import/no-unresolved */
+// @ts-ignore
+import { useStore } from "@stagepass/util-state";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  const store = useStore();
+
+  return (
+    <section>
+      {props.name} is mounted! --- {JSON.stringify(store)}
+    </section>
+  );
 }
