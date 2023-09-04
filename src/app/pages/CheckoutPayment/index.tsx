14a87fdfbc963275f40c15aeb86bceb686cbf28c
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-09-04 08:15:58 -0300

    feat: craete checkout stripe implementation

---
+++ src/app/pages/CheckoutPayment/index.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/prefer-default-export */
+// @ts-nocheck
+import { loadStripe } from "@stripe/stripe-js";
+import { useEffect, useState } from "react";
+import { useStore } from "@stagepass/util-state";
+import { navigateToUrl } from "single-spa";
+import { Loading } from "@stagepass/osiris-ui";
+
+let stripePromise: Promise<any>;
+const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(
+      "pk_test_51NV2XnGTOYfIjEnG2GNrGrZHLoCNvRSfgxaJtQMvQpxunIMG9SCGNoVWbmuoQlTyVWXv8D3aTipO6YKN0WnE2Ohd00ZKQcPJ3h"
+    );
+  }
+
+  return stripePromise;
+};
+
+export function CheckoutPayment() {
+  const store = useStore();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const item = {
+    price: store.ticketInfo.ticketOptions[0].id,
+    quantity: 1,
+  };
+
+  const checkoutSettings = {
+    lineItems: [item],
+    mode: "payment",
+    successUrl: "https://stagepasstickets.com/success",
+    cancelUrl: "https://stagepasstickets.com/cancel",
+  };
+
+  const redirectToCheckout = async () => {
+    setIsLoading(true);
+    const stripe: any = await getStripe();
+    const { error } = await stripe.redirectToCheckout(checkoutSettings);
+    if (error) {
+      console.warn("Error:", error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    redirectToCheckout(); // Call redirectToCheckout when the component mounts
+  }, []);
+
+  return isLoading ? <Loading /> : null;
+}
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,17 +1,7 @@
 /* eslint-disable import/no-unresolved */
 // @ts-ignore
-import { useStore } from "@stagepass/util-state";
+import { CheckoutPayment } from "./app/pages/CheckoutPayment";
 
 export default function Root(props) {
-  const store = useStore();
-
-  return (
-    <section
-      style={{
-        background: "white",
-      }}
-    >
-      {props.name} is mounted! --- {JSON.stringify(store)}
-    </section>
-  );
+  return <CheckoutPayment />;
 }
